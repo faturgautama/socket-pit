0a51376f086fe231956a32a5e4263acc81032759
Andre Kurniawan <andrekurniawan0891@gmail.com>
2022-01-20 11:58:54 +0700

    add event pis:update-antrian

--- client.js
+++ client.js
@@ -18,12 +18,18 @@ socket.on('connect', (data) => {
 //     );
 // }, 5000);
 
+const tiket = {
+    no_register: '000999',
+    nama_pasien: 'TANJIRO KAMADO',
+    nama_poli: 'POLI DALAM',
+};
+
 setTimeout(() => {
     socket.emit(
         'print:ticket-admisi-irja',
         JSON.stringify({
-            toid: 'd56de9811bb29388bc0ad9891212d32cab837c3df5b1ce3d96320091d5f493e8',
-            msg: 'from 1234 to 567',
+            toid: 'c24668b1bf53ef5ddf190f656144b75c827f6511387c31a4f217de778e1fa988',
+            msg: tiket,
         })
     );
 }, 5000);
--- server.js
+++ server.js
@@ -76,4 +76,22 @@ io.on('connection', (socket) => {
             }
         }
     });
+
+    socket.on('pis:update-antrian', (data) => {
+        var objData = JSON.parse(data);
+
+        if (clients.length > 0) {
+            let result = clients.filter((obj) => {
+                return obj.customId === objData.toid;
+            });
+
+            if (result[0] !== undefined) {
+                console.log(result[0].clientId);
+                console.log(objData.msg);
+                socket.broadcast
+                    .to(result[0].clientId)
+                    .emit('pis:update-antrian', objData.msg);
+            }
+        }
+    });
 });
